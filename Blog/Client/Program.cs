ef6a91174a4f3cad850115107e3a3392553687c9
nixzaihraiev <dmytro.zaihraiev@nixs.com>
2024-03-06 16:16:21 +0100

    Complete course work with gRPC.

---
+++ Blog/Client/Program.cs
@@ -0,0 +1,117 @@
+﻿using Blog;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace Client_gRPC
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel("localhost", 50050, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+            });
+
+            var client = new BlogService.BlogServiceClient(channel);
+
+            //var newBlog = CreateBlog(client);
+            //ReadBlog(client);
+            //UpdateBlog(client, newBlog);
+            //DeleteBlog(client);
+
+            await ListBlog(client);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+
+        private static Blog.Blog CreateBlog(BlogService.BlogServiceClient client)
+        {
+            var response = client.CreateBlog(new CreateBlogRequest()
+            {
+                Blog = new Blog.Blog
+                {
+                    AuthorId = "Dmytro",
+                    Title = "New blog!", 
+                    Content = "Hello world!"
+                }
+            });
+
+            Console.WriteLine("The blog " + response.Blog.Id + " was created");
+
+            return response.Blog;
+        }
+
+        private static void ReadBlog(BlogService.BlogServiceClient client)
+        {
+            try
+            {
+                var response = client.ReadBlog(new ReadBlogRequest()
+                {
+                    BlogId = "65e8708bd56d23f7d347fa3a"
+                });
+
+                Console.WriteLine(response.Blog.ToString());
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static void UpdateBlog(BlogService.BlogServiceClient client, Blog.Blog blog)
+        {
+            try
+            {
+                blog.AuthorId = "Updated author";
+                blog.Title = "Updated title";
+                blog.Content = "Updated content";
+
+                var response = client.UpdateBlog(new UpdateBlogRequest()
+                {
+                    Blog = blog
+                });
+
+                Console.WriteLine(response.Blog.ToString());
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static void DeleteBlog(BlogService.BlogServiceClient client)
+        {
+            try
+            {
+                var response = client.DeleteBlog(new DeleteBlogRequest()
+                {
+                    BlogId = "65e8708bd56d23f7d347fa3a"
+                });
+
+                Console.WriteLine("Blog with id " + response.BlogId + "was successfully deleted.");
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine(ex.Status.Detail);
+            }
+        }
+
+        private static async Task ListBlog(BlogService.BlogServiceClient client)
+        {
+            var response = client.ListBlog(new ListBlogRequest() { });
+
+            while(await response.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(response.ResponseStream.Current.Blog.ToString());
+            }
+        }
+    }
+}
---
+++ Blog/Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using Blog;
+using Grpc.Core;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
+using Server_gRPC.Services;
+using System;
+
+namespace Server_gRPC
+{
+    internal class Program
+    {
+        const int PORT = 50050;
+
+        static void Main(string[] args)
+        {
+            var reflectionServiceImpl = new ReflectionServiceImpl(BlogService.Descriptor, ServerReflection.Descriptor);
+
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", PORT, ServerCredentials.Insecure) },
+                    Services = { BlogService.BindService(new BlogServiceImpl()), 
+                        ServerReflection.BindService(reflectionServiceImpl)}
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on the port: " + PORT);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The server failed to start: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                if(server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
